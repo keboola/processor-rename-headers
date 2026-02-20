06004cf15da0fb7ef7506cbbbb69b52419be85fe
Josef Kudera <46950237+kudj@users.noreply.github.com>
2026-02-20 12:54:54 +0100

    fix tests typed

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -5,10 +5,14 @@ from datadirtest import DataDirTester
 
 class TestComponent(unittest.TestCase):
     def test_functional(self):
+        os.environ["KBC_STACKID"] = "connection.keboola.com"
+
         functional_tests = DataDirTester()
         functional_tests.run()
 
     def test_functional_types(self):
+        os.environ["KBC_STACKID"] = "connection.keboola.com"
+        os.environ['KBC_PROJECT_FEATURE_GATES'] = 'queuev2;someotherfeature'
         os.environ["KBC_DATA_TYPE_SUPPORT"] = "authoritative"
         functional_tests = DataDirTester(data_dir="./tests/functional_dtypes")
         functional_tests.run()
