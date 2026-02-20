5e4bc02f5a105d2f2cdd357a7bf0964dd05ddf73
Josef Kudera <46950237+kudj@users.noreply.github.com>
2026-02-20 11:32:27 +0100

    ruff

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -4,14 +4,13 @@ from datadirtest import DataDirTester
 
 
 class TestComponent(unittest.TestCase):
-
     def test_functional(self):
         functional_tests = DataDirTester()
         functional_tests.run()
 
     def test_functional_types(self):
-        os.environ['KBC_DATA_TYPE_SUPPORT'] = 'authoritative'
-        functional_tests = DataDirTester(data_dir='./tests/functional_dtypes')
+        os.environ["KBC_DATA_TYPE_SUPPORT"] = "authoritative"
+        functional_tests = DataDirTester(data_dir="./tests/functional_dtypes")
         functional_tests.run()
 
 
